1098d027844abd6edfec5e03cb10db490feb5db1
tamara bazaeva <tamara.bazaeva95@yandex.ru>
2023-11-10 16:59:02 +0300

    123

---
+++ src/pages/main/favorite.tsx
@@ -0,0 +1,14 @@
+function Favorites() : JSX.Element{
+  return (
+    <div className="page__favorites-container container">
+      <section className="favorites">
+        <h1 className="favorites__title">Saved listing</h1>
+        <ul className="favorites__list">
+
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Favorites;
