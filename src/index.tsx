5fbd84796321077279ba3374916671d7cabb86e0
tamara bazaeva <tamara.bazaeva95@yandex.ru>
2024-03-04 13:20:00 +0300

    map new 10

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import {offersData, reviewsData} from './mocks/offers-mocks';
-import { ReviewsType, OffersTypes } from './types';
+import {offersData, } from './mocks/offers-mocks';
+import { OffersTypes, ReviewsTypes } from './types';
+
+const reviewsData: ReviewsTypes = [
+  {
+    id: 134,
+    avatar: 'img/avatar.svg',
+    name: 'Alex',
+    raiting: 3434,
+    date: 14072020,
+    text: 'good choice',
+  },
+  {
+    id: 567,
+    avatar: 'img/avatar.svg',
+    name: 'Leo',
+    raiting: 3474,
+    date: 14020,
+    text: 'bad choice',
+  },
+  { id: 76,
+    avatar: 'img/avatar.svg',
+    name: 'Ramy',
+    raiting: 3554,
+    date: 14074420,
+    text: 'good choice',
+  },
+  {
+    id: 78,
+    avatar: 'img/avatar.svg',
+    name: 'Moty',
+    raiting: 994,
+    date: 1436320,
+    text: 'good choice',
+  }
+
+
+];
+
 
 type SettingsType = {
   numberOfRentalOffers: number;
   offers: OffersTypes;
-  reviews: ReviewsType[];
+  reviews: ReviewsTypes;
 }
 
 export const Settings: SettingsType = {
